6c0d6bc9e9a1c6398bd593814605b867219ce183
Imp0ster <90572478+the-imp0ster@users.noreply.github.com>
2023-08-03 17:15:29 -0300

    Add files via upload

---
+++ image_rename.py
@@ -0,0 +1,34 @@
+#. . . . . . . . . . . . . . . ╰──╮ NUMERICAL IMAGE RENAMER
+
+# . . . . . ╰──╮ INSTRUCTIONS:
+# . . . . . ╰──╮ to run this program, provide the path to your directory for the directory_path variable
+# . . . . . ╰──╮ then, open a terminal and run the following command:
+# . . . . . ╰──╮ python image_rename.py
+
+
+# . . . . . ╰──╮ imports.
+import os
+
+# . . . . . ╰──╮ provide the directory path where your image files are located.
+directory_path = "path/to/your/images"
+
+# . . . . . ╰──╮ list the image files found (file extensions .jpg, .jpeg, .png, .gif).
+image_files = [f for f in os.listdir(directory_path) if f.lower().endswith(('.jpg', '.jpeg', '.png', '.gif'))]
+
+# . . . . . ╰──╮ sort the list of image files for most cohesive renaming.
+image_files.sort()
+
+# . . . . . ╰──╮ iterate over, and numerically rename, the image files.
+for index, old_filename in enumerate(image_files, start=1):
+    file_extension = os.path.splitext(old_filename)[-1]
+    new_filename = f"{index}{file_extension}"
+    old_path = os.path.join(directory_path, old_filename)
+    new_path = os.path.join(directory_path, new_filename)
+    
+    # . . . . . ╰──╮ rename the image file.
+    os.rename(old_path, new_path)
+    print(f"Renamed {old_filename} to {new_filename}")
+    
+# . . . . . ╰──╮ inform the user that the renaming is complete.    
+print("Renaming complete.")
+
